30a7b737716903f70c2a74e7e08873f37d1e7ad1
Swasti  Deepan Das <swastideepandas@gmail.com>
2026-02-04 11:28:05 +0530

    change

---
+++ code.js
@@ -0,0 +1,13 @@
+console.log("Hello, World!"); 
+let name="Swasti";
+let age=21;
+let contact=9827331667;
+let address="Puri, odisha";
+
+let user={
+    name:"Swasti",
+    age:21,
+    contact:9827331667,
+    address:"Puri, odisha"
+}
+console.log(user.name);

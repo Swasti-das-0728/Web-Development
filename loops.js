9ccc22727ec4b2d7fbde35d39a7648a3589e8b93
Swasti  Deepan Das <swastideepandas@gmail.com>
2026-02-18 11:05:51 +0530

    add

--- loops.js
+++ loops.js
@@ -1,35 +1,42 @@
 
-// // // for (let i = 0; i < 9; i++) {
-// // //     console.log(i);
+// // // // for (let i = 0; i < 9; i++) {
+// // // //     console.log(i);
+// // // // }
+
+
+// // // // !
+
+
+// // // let a = 4;
+// // // let b = 5;
+// // // let sum = a + b;
+// // // // console.log(sum);
+
+// // // function divide(a, b) {
+// // //     console.log(a / b);
 // // // }
+// // // divide(10, 2)
 
-
-// // // !
-
-
-// // let a = 4;
-// // let b = 5;
-// // let sum = a + b;
-// // // console.log(sum);
-
-// // function divide(a, b) {
+// // function abc(a, b) {
+// //     console.log(a + b);
+// //     console.log(a - b);
+// //     console.log(a * b);
 // //     console.log(a / b);
 // // }
-// // divide(10, 2)
-
-// function abc(a, b) {
+// // abc(10, 2)
+// let add = function (a, b) {
 //     console.log(a + b);
-//     console.log(a - b);
-//     console.log(a * b);
-//     console.log(a / b);
 // }
-// abc(10, 2)
-let add = function (a, b) {
-    console.log(a + b);
-}
-add(10, 20);
+// add(10, 20);
 
-let subtract = function (a, b) {
-    console.log(a - b);
+// let subtract = function (a, b) {
+//     console.log(a - b);
+// }
+// subtract(10, 2);
+
+let addition = function (a = 43, b = 43) {
+    console.log(a + b);
+    console.log(a / b);
+
 }
-subtract(10, 2);
+addition()

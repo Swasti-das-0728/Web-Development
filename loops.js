31bc06ae5e230feeafb71b296b24696efaab8c32
Swasti  Deepan Das <swastideepandas@gmail.com>
2026-02-18 10:13:40 +0530

    add

--- loops.js
+++ loops.js
@@ -1,7 +1,35 @@
 
-for (let i = 0; i < 9; i++) {
-    console.log(i);
+// // // for (let i = 0; i < 9; i++) {
+// // //     console.log(i);
+// // // }
+
+
+// // // !
+
+
+// // let a = 4;
+// // let b = 5;
+// // let sum = a + b;
+// // // console.log(sum);
+
+// // function divide(a, b) {
+// //     console.log(a / b);
+// // }
+// // divide(10, 2)
+
+// function abc(a, b) {
+//     console.log(a + b);
+//     console.log(a - b);
+//     console.log(a * b);
+//     console.log(a / b);
+// }
+// abc(10, 2)
+let add = function (a, b) {
+    console.log(a + b);
 }
+add(10, 20);
 
-
-!
+let subtract = function (a, b) {
+    console.log(a - b);
+}
+subtract(10, 2);

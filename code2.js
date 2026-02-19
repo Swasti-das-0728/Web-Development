df3327cbd6020deadb9e96727aea9afc7a1720c1
Swasti  Deepan Das <swastideepandas@gmail.com>
2026-02-19 11:21:51 +0530

    add

---
+++ code2.js
@@ -0,0 +1,6 @@
+let fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits[0]);
+// fruits.push( "Orange")
+// console.log(fruits);
+fruits.pop()
+console.log(fruits);
--- loops.js
+++ loops.js
@@ -6,8 +6,7 @@
 
 // // // // !
 
-
-// // // let a = 4;
+ // // let a = 4;
 // // // let b = 5;
 // // // let sum = a + b;
 // // // // console.log(sum);

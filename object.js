fd139e2d3c797c412fecb41b39b23361ad4efebb
Swasti  Deepan Das <swastideepandas@gmail.com>
2026-02-25 10:48:58 +0530

    add

---
+++ object.js
@@ -0,0 +1,22 @@
+// let student = {
+//     name: "Swasti",
+//     branch: "B.Tech DAML",
+//     year: 3,
+//     cgpa: 8.9,
+//     college: "Centurion University"
+// };
+// console.log(student);
+// student.cgpa = 8.9;
+// console.log(student);
+// student.address = {
+//     city: "puri",
+//     state: "Odisha"
+// };      
+
+let person = {
+    name: "Swasti",
+    age: 21,}
+    location : {
+        postalCode: 752002,
+    }
+console.table(person);

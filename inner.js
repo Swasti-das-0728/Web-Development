d2fc1f93c4f8f8acfd46eca30eb449bb3fe57dcb
Swasti  Deepan Das <swastideepandas@gmail.com>
2026-02-26 10:58:04 +0530

    add

---
+++ inner.js
@@ -0,0 +1,5 @@
+let a = document.getElementsByClassName("header2")[0];
+a.innerText = "downloaded";
+console.dir(a);
+let header = document.getElementById("header2");
+header.innerHTML = "<i>Hey section F<i>"
